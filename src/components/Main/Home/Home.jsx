a241e32de46561ca152783528779a8f37fb16c97
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-07-20 17:03:38 +0400

    created radial circle for background

--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -4,25 +4,25 @@ import "./Home.scss";
 
 const Home = () => {
   return (
-    <article className="home_container">
-      <section className="aviato_studio_info">
-        <h1 className="aviator_studio_head">Aviator Studio –</h1>
-        <p className="aviator_studio_game">
-          The Original Crash Game Experience
-        </p>
-        <p className="aviator_studio">
-          Aviator Studio is a game provider focused on delivering engaging and
-          innovative gaming experiences, starting with the game Aviator. This
-          game is crafted with a unique multiplayer experience, thrilling
-          high-RTP gameplay, and an engaging visual & sound design that sets it
-          apart.
-        </p>
-        <Button variant="contact_us">Contact Us</Button>
-      </section>
-      <section className="aviator_studio_icon">
-        <img src={Plane} alt="aviator studio airplane" className="airplane"/>
-      </section>
-    </article>
+      <article className="home_container">
+        <section className="aviato_studio_info">
+          <h1 className="aviator_studio_head">Aviator Studio –</h1>
+          <p className="aviator_studio_game">
+            The Original Crash Game Experience
+          </p>
+          <p className="aviator_studio">
+            Aviator Studio is a game provider focused on delivering engaging and
+            innovative gaming experiences, starting with the game Aviator. This
+            game is crafted with a unique multiplayer experience, thrilling
+            high-RTP gameplay, and an engaging visual & sound design that sets
+            it apart.
+          </p>
+          <Button variant="contact_us">Contact Us</Button>
+        </section>
+        <section className="aviator_studio_icon">
+          <img src={Plane} alt="aviator studio airplane" className="airplane" />
+        </section>
+      </article>
   );
 };
 
---
+++ src/components/RadialGeadient/Radial.jsx
@@ -0,0 +1,10 @@
+import "./Radial.scss"
+
+const Radial = () => {
+  return (
+    <div className="radial_container">
+    </div>
+  )
+}
+
+export default Radial
