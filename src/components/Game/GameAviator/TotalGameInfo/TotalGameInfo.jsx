7ad12341d230eda508654c4a77a19c2ed487aed1
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-07-28 21:47:59 +0400

    total bets and total win section is done

--- src/components/Game/GameAviator/TotalGameInfo/TotalGameInfo.jsx
+++ src/components/Game/GameAviator/TotalGameInfo/TotalGameInfo.jsx
@@ -1,9 +1,22 @@
-import './TotalGameInfo.scss'
+import "./TotalGameInfo.scss";
 
 const TotalGameInfo = () => {
   return (
-    <div>TotalGameInfo</div>
-  )
-}
+    <article className="total_game_info">
+      <section className="bets_container">
+        <p className="bets_text">Bets</p>
+        <div className="total_bets_cont">
+          <p className="current_bets">100</p>
+          <p>/</p>
+          <p className="total_bets">100</p>
+        </div>
+      </section>
+      <section className="total_win_gel">
+        <p className="total_win_text">Total Win Gel</p>
+        <p className="total_gel">0</p>
+      </section>
+    </article>
+  );
+};
 
-export default TotalGameInfo
+export default TotalGameInfo;
